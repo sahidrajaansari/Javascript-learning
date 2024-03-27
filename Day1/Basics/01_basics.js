86da761cd447829bed2bd62bf1d60760323358b3
sahidrajaansari <94047034+sahidrajaansari@users.noreply.github.com>
2024-03-27 09:14:45 +0000

    Added 01_basics

---
+++ Day1/Basics/01_basics.js
@@ -0,0 +1,8 @@
+const facultyNumber="20COB335"
+let rollNumber=15
+var name="Sahid"
+hobby="Playing Cricket"
+let college;
+/*var Should Not be used due to its block scope and function scope*/
+
+console.table([facultyNumber,rollNumber,name,hobby,college]);
