4b74c1f68b54d61e9eb09e7b903f3efa640b697c
sahidrajaansari <94047034+sahidrajaansari@users.noreply.github.com>
2024-05-03 04:33:44 +0000

    Array is Over

---
+++ Day2/02_Array_Functions.js
@@ -0,0 +1,26 @@
+const marvelHeroes= ['Captain America','IronMan','Hulk']
+const dcHeroes=['Superman','Batman']
+// console.log(`A :Marvel Heroes :${marvelHeroes} And DC Heroes :${dcHeroes}`);
+// marvelHeroes.push(dcHeroes)
+// console.log(marvelHeroes);
+// console.log(`B :Marvel Heroes :${marvelHeroes} And DC Heroes :${dcHeroes}`);
+
+// const newArray=marvelHeroes.concat(dcHeroes)
+// console.log(newArray)
+
+// Spread Function
+// const newArrayWIthAllHeroes=[...marvelHeroes,...dcHeroes]
+// console.log(newArrayWIthAllHeroes);
+
+// const multiDimensionalArray=[1,2,3,[10,20,30],[10,20,[300,400,500]]]
+// console.log(multiDimensionalArray.flat(Infinity));
+
+console.log(Array.isArray("BhaiyaJi"))
+console.log(Array.from("BhaiyaJi"))
+
+let score1=100
+let score2=200
+let score3=300
+
+console.log(Array.of(score1,score2,score3));
+
