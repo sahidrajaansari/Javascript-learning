be596e992fc4ec93d377c3b7be1d0278bd33d07a
sahidrajaansari <94047034+sahidrajaansari@users.noreply.github.com>
2024-05-03 09:20:40 +0000

    Added video number 18

---
+++ Day2/05_JSON.js
@@ -0,0 +1,9 @@
+const flat={
+    ownerName:"Baua",
+    houseNumber:120,
+    familyIncome:123424n
+}
+const {familyIncome : fIncome} =flat
+console.log(typeof flat.familyIncome);
+// console.log(familyIncome);
+console.log(fIncome);
